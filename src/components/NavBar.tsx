720c93cd958cbf99dcccbcca180f4674eed3bd52
Vishwa Shetty <vishwa.shetty.99@gmail.com>
2024-12-02 01:17:20 +0530

    Removed custom theme context

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,15 @@
 import { HStack, Image, Img, Input, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import user from "../assets/user.png";
-import { useContext, useEffect } from "react";
-import { ThemeContext } from "../context/ThemeContext";
 
 const NavBar = () => {
-  const { theme, toggleTheme } = useContext(ThemeContext);
-  useEffect(() => {
-    document.body.className = theme;
-  }, [theme]);
-
   return (
     <>
       <HStack spacing={5}>
         <Img src={logo} boxSize="50px" />
         <Text>Vishwa Game Hub</Text>
         <Input />
-        <Text>{theme}</Text>
-        <Image src={user} width="30px" onClick={() => toggleTheme()} />
+        <Image src={user} width="30px" onClick={() => console.log("clicked")} />
       </HStack>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import ThemeProvider from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );

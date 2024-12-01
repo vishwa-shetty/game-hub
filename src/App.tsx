84f15529a097f02b1a0812b2ae32e2900c92e539
Vishwa Shetty <vishwa.shetty.99@gmail.com>
2024-12-01 19:43:01 +0530

    Added chkra UI and common component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import { Grid, GridItem } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <Grid
+        templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+        gridTemplateRows={"50px 1fr 30px"}
+        gridTemplateColumns={"150px 1fr"}
+        h="100vh"
+        gap="1"
+        color="blackAlpha.700"
+        fontWeight="bold"
+      >
+        <GridItem pl="2" bg="orange.300" area={"header"}>
+          <NavBar />
+        </GridItem>
+        <GridItem pl="2" bg="pink.300" area={"nav"}>
+          <SideBar />
+        </GridItem>
+        <GridItem pl="2" bg="green.300" area={"main"}>
+          <Main />
+        </GridItem>
+        <GridItem pl="2" bg="blue.300" area={"footer"}>
+          <Footer />
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
-export default App
+export default App;

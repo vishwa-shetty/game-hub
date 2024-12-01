1f69f780ee92d1abfadea88ed4d51ec3fa97a7ed
Vishwa Shetty <vishwa.shetty.99@gmail.com>
2024-12-01 21:09:18 +0530

    completed sidebar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,15 @@
-import { HStack, Img, Text } from "@chakra-ui/react";
+import { Button, HStack, Image, Img, Input, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
+import user from "../assets/user.png";
 
 const NavBar = () => {
   return (
     <>
-      <HStack>
+      <HStack spacing={5}>
         <Img src={logo} boxSize="50px" />
         <Text>Vishwa Game Hub</Text>
+        <Input />
+        <Image src={user} width="30px" />
       </HStack>
     </>
   );

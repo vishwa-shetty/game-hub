a907434b66545a49b8f90910d6b70f838e1c2c80
Vishwa Shetty <vishwa.shetty.99@gmail.com>
2024-12-06 14:18:45 +0530

    fixed footer text

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,20 @@
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Divider, Heading, HStack, Link } from "@chakra-ui/react";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <Divider margin="20px" />
+      <HStack justifyContent="center" alignItems="center">
+        <Heading fontSize="12px">
+          Design and Developed by- Vishwa Shetty
+        </Heading>
+        <Link href="https://www.linkedin.com/in/vishwa-kalshetty/" isExternal>
+          <ExternalLinkIcon mx="2px" />
+        </Link>
+      </HStack>
+    </>
+  );
 };
 
 export default Footer;

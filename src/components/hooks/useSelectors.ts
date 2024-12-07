8b3096f8b9ab73d0424b5db2aa471853469b5090
Vishwa Shetty <vishwa.shetty.99@gmail.com>
2024-12-07 16:07:02 +0530

    regactor created resuable api client

--- src/components/hooks/useSelectors.ts
+++ src/components/hooks/useSelectors.ts
@@ -5,25 +5,15 @@ import {
   Genres,
   Platform,
 } from "../../models/games";
-import { genresData } from "../data/genresData";
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client";
+import { getAll } from "../services/api-client";
+import { genresData } from "../data/genresData";
 import { platformData } from "../data/platformData";
 
 export const useGames = (gameQuery: GameQuery) =>
   useQuery<FetchResponse<Games>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Games>>("/games", {
-          params: {
-            genres: gameQuery?.genre?.id,
-            parent_platforms: gameQuery?.platform?.id,
-            ordering: gameQuery?.sort?.value,
-            search: gameQuery?.search,
-          },
-        })
-        .then((res) => res.data),
+    queryFn: () => getAll<Games>("/games", gameQuery),
     staleTime: 24 * 60 * 60 * 1000, //24H
   });
 
@@ -31,20 +21,15 @@ export const useGames = (gameQuery: GameQuery) =>
 export const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genres>>("/genres").then((res) => res.data),
+    queryFn: () => getAll<Genres>("/genres"),
     staleTime: 24 * 60 * 60 * 1000, //24H
     initialData: genresData,
   });
 
-// fetching parent platform
 export const usePlatform = () =>
   useQuery({
     queryKey: ["platforms"],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Platform>>("/platforms/lists/parents")
-        .then((res) => res.data),
+    queryFn: () => getAll<Platform>("/platforms/lists/parents"),
     staleTime: 24 * 60 * 60 * 1000, //24H
     initialData: platformData,
   });
--- src/components/services/api-client.ts
+++ src/components/services/api-client.ts
@@ -1,6 +1,19 @@
 import axios from "axios";
+import { FetchResponse, GameQuery } from "../../models/games";
 
-export default axios.create({
+const apiService = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: { key: "ec71a87c1022492196d10a928da946c0" },
 });
+
+export const getAll = <T>(endpoint: string, gameQuery?: GameQuery) =>
+  apiService
+    .get<FetchResponse<T>>(endpoint, {
+      params: {
+        genres: gameQuery?.genre?.id,
+        parent_platforms: gameQuery?.platform?.id,
+        ordering: gameQuery?.sort?.value,
+        search: gameQuery?.search,
+      },
+    })
+    .then((res) => res.data);

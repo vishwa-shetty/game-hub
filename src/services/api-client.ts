c5c5e898b1563aa6de1fd8d729fc1bd8a6094ba7
Vishwa Shetty <vishwa.shetty.99@gmail.com>
2024-12-13 13:36:42 +0530

    added env file and example file to project

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { FetchResponse } from "../models/games";
+const gameKey = import.meta.env.VITE_RAW_API_KEY;
 
 const apiService = axios.create({
   baseURL: "https://api.rawg.io/api",
-  params: { key: "ec71a87c1022492196d10a928da946c0" },
+  params: { key: gameKey },
 });
 
 export const getAll = <T>(endpoint: string, config?: AxiosRequestConfig) =>

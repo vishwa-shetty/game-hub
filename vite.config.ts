c45760909260d6d838b707688fb3851046c29eb1
Vishwa Shetty <vishwa.shetty.99@gmail.com>
2024-12-15 16:06:27 +0530

    reduced bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/game-over/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"], // Moves React and ReactDOM to a separate chunk
+          ui: ["./src/pages/GameDetailsPage", "./src/components/game/GameCard"], // Example grouping for heavy UI components
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600, // Optional: Adjust warning limit
+  },
 });
